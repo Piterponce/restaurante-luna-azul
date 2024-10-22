3934eef6173b9f3048444762c2092cba4aa9d100
Piter Ponce <110115950+Piterponce@users.noreply.github.com>
2024-10-22 18:58:47 -0500

    AAAAAAAAAAAA

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
+const path = require('path'); // Asegúrate de importar el módulo 'path'
 const app = express();
-const PORT = 10000;
+const PORT = process.env.PORT || 10000;
 
-
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname))); // Corregido 'pp' a 'app'
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-  });
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
